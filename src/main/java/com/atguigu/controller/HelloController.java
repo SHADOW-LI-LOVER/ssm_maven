e72a4cba2b27e2dc85a9a408ce6d35b005ee10cf
shadow0821 <shaow@163.com>
2020-12-09 21:00:09 +0800

    idea_test01

--- src/main/java/com/atguigu/controller/HelloController.java
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.atguigu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
