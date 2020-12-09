8df8bc0bacbc085b45c16746e805c68f1586c9b1
SHADOW-LI-LOVER <75706094+SHADOW-LI-LOVER@users.noreply.github.com>
2020-12-09 20:58:06 +0800

    Update HelloController.java

--- src/main/java/com/atguigu/controller/HelloController.java
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -1,4 +1,11 @@
 package com.atguigu.controller;
 
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
+  
+  @RequestMapping("/show")
+  public String show(){
+    return "hei hei hei";
+  }
 }
